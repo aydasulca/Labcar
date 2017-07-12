f367706bd9328b9d84aea2d2adf3d84d2e3561ef
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-07-11 23:03:49 -0500

    proyecto-portafolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,67 @@
-function initMap() {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 6
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+var containerMap = document.getElementById("map");
+var inpOrigin = document.getElementById("inp-origin");
+var inpDestiny = document.getElementById("inp-destiny");
+var btnRoute = document.getElementById("btn-route");
+var map;
+var marker;
+function initMap(){
+  var barranco = {lat: -12.143932, lng: -77.021874};
+  map= new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: barranco
+  });
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  var markBarranco = new google.maps.Marker({
+    animation: google.maps.Animation.BOUNCE,
+    position: barranco,
+    map: map
+  });
+  (navigator.geolocation)? navigator.geolocation.getCurrentPosition(seePosition): containerMap.innerHTML = "Geolocalización no es soportado por tu navegador";
+  autocompleteInput();
+  btnRoute.addEventListener("click", showRoute);
+}
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-            map.setZoom(18);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
+function seePosition(position){
+  var pos= {lat: position.coords.latitude,
+            lng: position.coords.longitude
           }
-      };
+  map = new google.maps.Map(containerMap, {
+    center: pos,
+    zoom: 16
+  });
+  marker = new google.maps.Marker({
+    position: pos,
+    map: map
+  });
+}
+
+function autocompleteInput(){
+  new google.maps.places.Autocomplete(inpOrigin);
+  new google.maps.places.Autocomplete(inpDestiny);
+}
+
+function showRoute(){
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+  var request = {
+    origin: inpOrigin.value,
+    destination: inpDestiny.value,
+    travelMode: "DRIVING",
+  };
+  directionsService.route(request, function(response, status){
+    if(status === 'OK'){
+      directionsDisplay.setDirections(response);
+      marker.setMap(null);
+      rateForTravel(response);
+    }else{
+      window.alert("No encontramos una ruta.");
+    }
+  })
+}
+
+function rateForTravel(response){
+  var distFromOriginToDest = response.routes[0].legs[0].distance.text.replace('km', '').replace(',', '.');
+  var rate = distFromOriginToDest*2;
+  var totalRate = document.getElementById("total-rate");
+  totalRate.innerHTML = (rate > 5)? "S/. " + parseInt(rate): "S/. 5";
+}
